fc7bd14d16698b760c48f6569da3620f87013f06
Sandesh Sandesh <sandesh711@gmail.com>
2025-07-29 20:31:49 +0530

    Add files via upload

---
+++ freedom_fighter.sql
@@ -0,0 +1,22 @@
+create or replace table freedom_fighters(
+    name varchar(30) PRIMARY KEY,
+    contribution varchar(10000)
+);
+insert into freedom_fighters values
+('Bal Gangadhar Tilak','Promoter of Swaraj and Swadeshi:
+Tilak championed the concept of self-rule and encouraged the use of Indian-made products, a movement that gained momentum following the Partition of Bengal. 
+Utilized "Kesari" and "Maratha":
+He leveraged his newspapers, "Kesari" and "Maratha," to spread nationalist ideas, foster collective thinking, and promote Swadeshi principles. 
+Established "Deccan Education Society":
+Through the "Deccan Education Society," he aimed to develop collective action and awareness among Indians. 
+"Swaraj is my birthright, and I shall have it":
+This iconic slogan encapsulated his unwavering demand for Indian independence and ignited a patriotic fervor among millions. '),
+('Sardar Vallabhbhai Patel','Leadership in Movements:
+Patel was a prominent leader in various freedom movements, including the Champaran Satyagraha, Kheda Satyagraha, and the Bardoli Satyagraha, where he inspired civil disobedience and demonstrated exemplary leadership against British oppression. 
+Unity and Diplomacy:
+He was a staunch advocate for the unity of India and demonstrated exceptional diplomatic skills and firmness in persuading princely states to join the Indian Union after independence.'),
+('Mahatma Gandhi','Leadership in Mass Movements:
+Gandhi led significant national movements that united a diverse Indian population under the common goal of independence. These included the Non-Cooperation Movement (1920-1922), the Salt Satyagraha (Dandi March, 1930), the Civil Disobedience Movement, and the Quit India Movement (1942). 
+Satyagraha and Nonviolent Resistance:
+Gandhi popularized and implemented the philosophy of Satyagraha (truth force) and Ahimsa (nonviolence) as powerful tools for achieving political change. This approach involved peaceful protests, civil disobedience, boycotts, and strikes, which put immense pressure on the British government.')
+ ;  
